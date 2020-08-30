059ecc7f0ec0ea821fa4aacbf3f41de80ab6e844
cebe01 <cedric.berger@outlook.com>
2020-08-30 14:05:15 +0200

    v.021

--- bau_evaluations.py
+++ bau_evaluations.py
@@ -7,56 +7,55 @@ Created on Fri Aug 28 15:27:21 2020
 
 import numpy as np
 import pandas as pd
-# baupy
-
+import baupy as bp
 #%%
 
-class baupy():
+# class baupy():
     
-    def __init__(self, path):
+#     def __init__(self, path):
         
-        self.path = path
-        self.values = pd.read_csv(path, sep='\t')
-        self.Raumgewicht = self.values.Raumgewicht
-        self.Mächtigkeit = self.values.Mächtigkeit
-        self.Ve = self.values.Ve
-        self.We = self.values.We
+#         self.path = path
+#         self.values = pd.read_csv(path, sep='\t')
+#         self.Raumgewicht = self.values.Raumgewicht
+#         self.Mächtigkeit = self.values.Mächtigkeit
+#         self.Ve = self.values.Ve
+#         self.We = self.values.We
         
         
-    def av_raumgewicht(self):
+#     def av_raumgewicht(self):
         
-        no_layers = len(self.values.Raumgewicht)
-        av_raumgewicht = np.zeros(no_layers)
-        thickness = np.zeros(no_layers)
-        thickness[0] = self.values.Mächtigkeit[0]
-        av_raumgewicht[0] = self.values.Raumgewicht[0]
+#         no_layers = len(self.values.Raumgewicht)
+#         av_raumgewicht = np.zeros(no_layers)
+#         thickness = np.zeros(no_layers)
+#         thickness[0] = self.values.Mächtigkeit[0]
+#         av_raumgewicht[0] = self.values.Raumgewicht[0]
         
-        for i in range(1,no_layers):
-            thickness[i] = self.Mächtigkeit[i] + thickness[i-1]
-            av_raumgewicht[i] = (self.Raumgewicht[i]*self.Mächtigkeit[i] + av_raumgewicht[i-1]*thickness[i-1])/thickness[i]
-        self.thickness = thickness
-        self.av_raumgewicht = av_raumgewicht
+#         for i in range(1,no_layers):
+#             thickness[i] = self.Mächtigkeit[i] + thickness[i-1]
+#             av_raumgewicht[i] = (self.Raumgewicht[i]*self.Mächtigkeit[i] + av_raumgewicht[i-1]*thickness[i-1])/thickness[i]
+#         self.thickness = thickness
+#         self.av_raumgewicht = av_raumgewicht
         
-    def Me(self):
+#     def Me(self):
         
-        sigma_atm = 100
-        Ve = self.Ve
-        We = self.We
-        thickness = self.thickness
-        av_raumgewicht = self.av_raumgewicht
-        Me_max = Ve*sigma_atm*(av_raumgewicht*thickness/sigma_atm)**We/1000
-        thick_min = np.zeros(len(thickness))
-        thick_min[1::] = thickness[0:-1]
-        Me_min = Ve*sigma_atm*(av_raumgewicht*thick_min/sigma_atm)**We/1000
-        self.Me_max = Me_max
-        self.Me_min = Me_min
+#         sigma_atm = 100
+#         Ve = self.Ve
+#         We = self.We
+#         thickness = self.thickness
+#         av_raumgewicht = self.av_raumgewicht
+#         Me_max = Ve*sigma_atm*(av_raumgewicht*thickness/sigma_atm)**We/1000
+#         thick_min = np.zeros(len(thickness))
+#         thick_min[1::] = thickness[0:-1]
+#         Me_min = Ve*sigma_atm*(av_raumgewicht*thick_min/sigma_atm)**We/1000
+#         self.Me_max = Me_max
+#         self.Me_min = Me_min
         
-    def save_kennwerte(self):
+#     def save_kennwerte(self):
         
-        to_save = pd.DataFrame(self.values)
-        to_save.loc[:,'Me_min'] = self.Me_min
-        to_save.loc[:,'Me_max'] = self.Me_max
-        to_save.to_excel('kennwerte.xlsx')
+#         to_save = pd.DataFrame(self.values)
+#         to_save.loc[:,'Me_min'] = self.Me_min
+#         to_save.loc[:,'Me_max'] = self.Me_max
+#         to_save.to_excel('kennwerte.xlsx')
         
         
         
@@ -68,7 +67,7 @@ class baupy():
 path = r'/home/cb/Dokumente/GitHub/baupy/kennwerte_streichholzstrasse.txt'
 
 
-container = baupy(path)
-container.av_raumgewicht()
+container = bp.kennwerte(path)
+#container.av_raumgewicht()
 container.Me()
 container.save_kennwerte()
